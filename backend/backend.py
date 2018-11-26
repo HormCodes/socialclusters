c0daed988630f2b75df5d86f761eed33c1611612
HormCZ <horak@horm.cz>
2018-11-26 11:13:05 +0100

    Got first data from Twitter

--- backend/backend.py
+++ backend/backend.py
@@ -1,11 +1,71 @@
+import sys
+
 import requests
+import json
 
-from config import twitterKey
+from pprint import pprint
 
-print(twitterKey)
+sys.path.append('/Library/Frameworks/Python.framework/Versions/3.7/lib/python3.7/site-packages')
+
+from requests_oauthlib import OAuth1
+
+CONFIG_JSON_FILE_NAME = "config.json"
+
+SOURCES_JSON_FILE_NAME = "config.json"
+TWITTER_KEY = "twitter"
+ACCOUNTS_KEY = "accounts"
+
+TWITTER_API_BASE_URL = "https://api.twitter.com/1.1/"
+
+
+# TODO - Test
+# TODO - Rename
+def get_twitter_accounts(sources):
+    return sources[TWITTER_KEY][ACCOUNTS_KEY]
+
+
+# TODO - Test
+def get_twitter_account_timeline_url(username):
+    return TWITTER_API_BASE_URL + "statuses/user_timeline.json?screen_name=" + username
+
+
+def get_twitter_keys(config):
+    return config["keys"][TWITTER_KEY]
+
+
+def get_twitter_access_token(twitter_keys):
+    return twitter_keys["accessToken"]
+
+
+def get_twitter_consumer(twitter_keys):
+    return twitter_keys["consumer"]
+
+
+with open(CONFIG_JSON_FILE_NAME) as file:
+    config = json.load(file)
+
+with open(SOURCES_JSON_FILE_NAME) as file:
+    sources = json.load(file)
+
+pprint(config)
+
+twitter_keys = get_twitter_keys(config)
+
+oauth = OAuth1(get_twitter_consumer(twitter_keys)["key"],
+                  get_twitter_consumer(twitter_keys)["secret"],
+            get_twitter_access_token(twitter_keys)["token"],
+                  get_twitter_access_token(twitter_keys)["secret"])
+
+session = requests.session()
+session.auth = oauth
+response = session.get(get_twitter_account_timeline_url("brnomycity"))
+
+results = json.loads(response.content)
+
+pprint(results)
 
 # TODO - Get Data
 
 # TODO - Remove stopwords (https://github.com/stopwords-iso/stopwords-iso)
 
-# TODO -
+# TODO - Print texts
