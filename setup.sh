d5a60d7a7e9c4003fe154e46958deb5f26ec257a
Horm <horak@horm.cz>
2019-04-28 21:58:24 +0200

    Add container for frontend

--- setup.sh
+++ setup.sh
@@ -9,5 +9,8 @@ docker build -t socialclusters/backend .
 cd ../ml_backend
 docker build -t socialclusters/ml_backend .
 
+cd ../frontend
+docker build -t socialclusters/frontend .
+
 cd ../docker
 docker-compose up -d
