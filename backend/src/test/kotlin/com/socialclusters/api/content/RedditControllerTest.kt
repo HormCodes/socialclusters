45458b11bef161496da8d2c47c09786b5bfa3d44
Horm <horak@horm.cz>
2019-04-29 15:34:41 +0200

    Fix order test

--- backend/src/test/kotlin/com/socialclusters/api/content/RedditControllerTest.kt
+++ backend/src/test/kotlin/com/socialclusters/api/content/RedditControllerTest.kt
@@ -1,6 +1,8 @@
 package com.socialclusters.api.content
 
 import com.socialclusters.domain.impl.RedditPostRepository
+import com.socialclusters.pojos.RedditPost
+import io.kotlintest.Description
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.DescribeSpec
 import org.springframework.boot.test.context.SpringBootTest
@@ -10,16 +12,25 @@ import org.springframework.data.domain.Sort
 @SpringBootTest
 class RedditControllerTest(
   redditController: RedditController,
-  redditPostRepository: RedditPostRepository
-) : DescribeSpec({
-  describe("getRedditContent") {
-    it("should for request with order return ordered content") {
-      val content = redditController.getTwitterContent(false, "", PageRequest(0, 20, Sort(Sort.Direction.DESC, "score")))
-      println(content)
+  val redditPostRepository: RedditPostRepository
+) : DescribeSpec() {
+  override fun beforeTest(description: Description) {
+    redditPostRepository.deleteAll()
+  }
 
-      val highestScore = redditPostRepository.findAll().toList().sortedByDescending { it.score }.first().score
+  init {
+    describe("getRedditContent") {
+      it("should for request with order return ordered content") {
+        redditPostRepository.insert(RedditPost("123", "123", null, null, "", "", "", "", "", "", 0, 0))
+        redditPostRepository.insert(RedditPost("124", "123", null, null, "", "", "", "", "", "", 1, 0))
 
-      content.first().score shouldBe highestScore
+        val content = redditController.getTwitterContent(false, "", PageRequest(0, 20, Sort(Sort.Direction.DESC, "score")))
+        println(content)
+
+        val highestScore = redditPostRepository.findAll().toList().sortedByDescending { it.score }.first().score
+
+        content.first().score shouldBe highestScore
+      }
     }
   }
-})
+}
