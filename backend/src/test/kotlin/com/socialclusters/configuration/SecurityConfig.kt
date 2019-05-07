940ddcd63e6ec15284c6700d827149e34b2184da
Horm <horak@horm.cz>
2019-05-07 11:45:34 +0200

    Fix testing conf for auth

---
+++ backend/src/test/kotlin/com/socialclusters/configuration/SecurityConfig.kt
@@ -0,0 +1,71 @@
+package com.socialclusters.configuration
+
+import com.socialclusters.services.CustomUserDetailsService
+import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.context.annotation.Bean
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.config.BeanIds
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+
+
+@TestConfiguration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true, jsr250Enabled = true, prePostEnabled = true)
+class SecurityConfig(
+  var customUserDetailsService: CustomUserDetailsService,
+  val unauthorizedHandler: JwtAuthenticationEntryPoint
+) : WebSecurityConfigurerAdapter() {
+
+  @Bean
+  fun jwtAuthenticationFilter(): JwtAuthenticationFilter {
+    return JwtAuthenticationFilter()
+  }
+
+  @Throws(Exception::class)
+  override fun configure(authenticationManagerBuilder: AuthenticationManagerBuilder) {
+    authenticationManagerBuilder
+      .userDetailsService(customUserDetailsService)
+      .passwordEncoder(passwordEncoder())
+  }
+
+  @Bean(BeanIds.AUTHENTICATION_MANAGER)
+  @Throws(Exception::class)
+  override fun authenticationManagerBean(): AuthenticationManager {
+    return super.authenticationManagerBean()
+  }
+
+  @Bean
+  fun passwordEncoder(): PasswordEncoder {
+    return BCryptPasswordEncoder()
+  }
+
+  @Throws(Exception::class)
+  override fun configure(http: HttpSecurity) {
+    http
+      .cors()
+      .and()
+      .csrf()
+      .disable()
+      .exceptionHandling()
+      .authenticationEntryPoint(unauthorizedHandler)
+      .and()
+      .sessionManagement()
+      .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+      .and()
+      .authorizeRequests()
+      .anyRequest()
+      .permitAll()
+
+    // Add our custom JWT security filter
+    http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter::class.java)
+
+  }
+}
